8c00a5a2474185781c9acd7b97382a789fc5b0e7
Askarbek <askarbekospan@gmail.com>
2019-02-16 13:54:06 +0600

    jfjkdfl

---
+++ lab2task4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathstring = @"C:\CreatingFile";
+            string path2 = @"C:\2file";
+            string filename = "Just.txt";
+
+            string loc = Path.Combine(pathstring, filename);           
+       File.Create(loc);
+
+            string loc2 = Path.Combine(path2, filename);
+        
+            File.Copy(loc, loc2, true);
+
+            File.Delete(loc);
+        }
+        
+    }
+}
