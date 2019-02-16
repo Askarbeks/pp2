21b49bd0420e5c4576e7828fbd420fe0e418b4ba
Askarbek <askarbekospan@gmail.com>
2019-02-16 13:56:52 +0600

    grhrh

---
+++ week2/task1/lab2.task1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\admin\Documents\infos.txt");
+         
+
+            bool b = true;
+            string[] chars = text.Split();
+            for (int i = 0; i < text.Length / 2; i++)
+            {
+              
+                if (text[i] == text[text.Length - 1-i])
+                {
+                    b = true;
+                }
+                else
+                {
+                    b = false;
+                }
+            }
+                if (b)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+
+            }
+         }
+    }
---
+++ week2/task2/lab2task2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\admin\Documents\lab2task2input.txt");
+
+            
+
+            string[] num = text.Split();
+            List<int> list = new List<int>();
+            
+
+            for(int i=0; i<num.Count(); i++)
+            {
+                int a = int.Parse(num[i]);
+            
+                    if (a == 2)
+                    {
+                        list.Add(a);
+                    }
+                    else
+                    {
+                        for (int j = 2; j < a; j++)
+                        {
+                            if (a % j == 0)
+                            {
+                                break;
+                            }
+                            if (j == a - 1)
+                            {
+                                list.Add(a);
+                            }
+                        }
+
+                    }
+                }
+            string res = "";
+                for (int i = 0; i < list.Count; i++)
+                {
+                 res = res + list[i] + " ";
+                          
+                }System.IO.File.WriteAllText(@"C:\Users\admin\Documents\lab2task2output.txt", res);              
+            }
+        }
+    }
